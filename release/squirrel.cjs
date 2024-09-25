5d5c7333aa782f0baddefa4ffcbacd33682bd33a
Dave Luhman <dave@ado.software>
2024-09-25 15:53:41 -0500

    Switch kiosk icon to higher resolution (1024px) for better clarity. Enhance Windows installer setup by adding a loading GIF, specifying authorship to Dave Luhman, and more metadata like application version, name, and icons. These improvements ensure a more professional installation experience.

--- release/squirrel.cjs
+++ release/squirrel.cjs
@@ -4,8 +4,14 @@ try {
     await electronInstaller.createWindowsInstaller({
       appDirectory: './out/ReloaderKiosk-win32-x64/',
       outputDirectory: './out/make/squirrel/',
-      authors: 'ADO Software',
-      exe: 'ReloaderKiosk.exe'
+      authors: 'Dave Luhman',
+      exe: 'ReloaderKiosk.exe',
+      loadingGif: "./img/kioskPulse.gif",
+      owners: 'ADO Software',
+      version: '1.0.1-Release',
+      name: "Reloader Kiosk",
+      iconUrl: './img/kiosk.ico',
+      setupIcon: "./img/kiosk.ico"
     });
   } catch (e) {
     console.log(`No dice: ${e.message}`);
